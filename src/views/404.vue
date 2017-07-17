a81f6959c5c2e321a7be8a47f7601d46f9161174
chpengzh <chpengzh@foxmail.com>
2017-07-18 01:20:20 +0800

    404跳转到博客路由

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,16 +1,31 @@
 <template>
   <div class="not-found">
-    <h1>404</h1>
-    <p>你来到什么都没有的处女地。</p>
+    <h1><a href="/">{{title}}</a></h1>
+    <p>{{body}}</p>
   </div>
 </template>
 
 <script>
+  export default{
+    data() {
+      return {
+        title: window.location.hostname,
+        body: '我们致力于提供最好的Kotlin中文教程, 共建最潮流的Kotlin中文社区',
+      }
+    },
+    created() {
+      if (!window.location.href.endsWith('/404')) {
+        window.location.href = window.location.href.replace(window.location.hostname, 'blog.kotliner.cn')
+      } else {
+        this.body = '你来到什么都没有的处女地。'
+      }
+    }
+  }
 </script>
 <style scoped lang="less">
-.not-found{
-  color: #999;
-  margin:20% auto;
-  text-align: center;
-}
+  .not-found {
+    color: #999;
+    margin: 20% auto;
+    text-align: center;
+  }
 </style>
